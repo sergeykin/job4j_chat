35fa061126a7ffa98152115f59e99ab4a7732b4f
sergeykin <sergeykinn@mail.ru>
2021-08-08 10:44:12 +0300

    6. ResponseEntity [#504799]

--- src/main/java/ru/job4j/chat/controller/RoleController.java
+++ src/main/java/ru/job4j/chat/controller/RoleController.java
@@ -1,13 +1,17 @@
 package ru.job4j.chat.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMapAdapter;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import ru.job4j.chat.model.Role;
 import ru.job4j.chat.repository.RoleRepository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -42,8 +46,14 @@ public class RoleController {
 
     @PostMapping("/")
     public ResponseEntity<Role> create(@RequestBody Role role) {
-        return new ResponseEntity<Role>(
-                this.roleRepository.save(role),
+        this.roleRepository.save(role);
+        Object body = new HashMap<>() {{
+            put("role_id", role.getId());
+            put("role_name", role.getName());
+        }};
+        return new ResponseEntity(
+                body,
+                new MultiValueMapAdapter<>(Map.of("role", List.of("job4j"))),
                 HttpStatus.CREATED
         );
     }
@@ -52,5 +62,32 @@ public class RoleController {
     public ResponseEntity<Void> update(@RequestBody Role role) {
         this.roleRepository.save(role);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/example3")
+    public ResponseEntity<?> example3() {
+        Object body = new HashMap<>() {{
+            put("key", "value11");
+            put("zzz", "zzz1");
+        }};
+        var entity = new ResponseEntity(
+                body,
+                new MultiValueMapAdapter<>(Map.of("Job4jCustomHeader", List.of("job4j"))),
+                HttpStatus.OK
+        );
+        return entity;
+    }
+
+    @GetMapping("/example4")
+    public ResponseEntity<String> example4() {
+        var body = new HashMap<>() {{
+            put("key", "value");
+        }}.toString();
+        var entity = ResponseEntity.status(HttpStatus.CONFLICT)
+                .header("Job4jCustomHeader", "job4j")
+                .contentType(MediaType.TEXT_PLAIN)
+                .contentLength(body.length())
+                .body(body);
+        return entity;
     }
 }
